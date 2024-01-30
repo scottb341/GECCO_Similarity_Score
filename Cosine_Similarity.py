f80828336faafee009521f4d468a5eedebd192eb
scottb341 <137969536+scottb341@users.noreply.github.com>
2024-01-30 11:29:10 +1100

    Update Cosine_Similarity.py

--- Cosine_Similarity.py
+++ Cosine_Similarity.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 23 18:03:03 2024
-
-@author: Scott Blyth
-"""
-
+import os
 import torch
-from transformers import BertTokenizer, BertModel
-from functools import lru_cache
+os.environ['TRANSFORMERS_CACHE'] = 'D:\Models'
+from transformers import RobertaTokenizer, RobertaModel
+
 
 CODE_BERT_MODEL = "microsoft/codebert-base"
-
 class SimilarityScore: 
     def __init__(self, model_name):
-        self.tokenizer = BertTokenizer.from_pretrained(model_name)
-        self.model = BertModel.from_pretrained(model_name,output_hidden_states=True )
+        self.tokenizer = RobertaTokenizer.from_pretrained(model_name)
+        self.model = RobertaModel.from_pretrained(model_name,output_hidden_states=True)
         self.model.eval()
 
     def cosine(self, v1,v2): 
@@ -26,8 +20,7 @@ class SimilarityScore:
         marked_text = "[CLS] " + text + " [SEP]"
         tokenized_text = self.tokenizer.tokenize(marked_text)
         return tokenized_text
-    
-    @lru_cache(maxsize=32, typed=False)
+
     def embed(self, text):
         tokenizer = self.tokenizer
         tokens = self.get_tokens(text)
@@ -40,11 +33,11 @@ class SimilarityScore:
         segments_tensors = torch.tensor([segments_ids])
     
         with torch.no_grad():
-            hidden_states,_ = self.model(tokens_tensor, segments_tensors)
+            outputs = self.model(tokens_tensor, segments_tensors)
+            hidden_states = outputs[2]
             
         token_vecs = hidden_states[-2][0]
-    
-        # Calculate the average of all 22 token vectors.
+
         sentence_embedding = torch.mean(token_vecs, dim=0)
         
         return sentence_embedding
@@ -53,3 +46,4 @@ class SimilarityScore:
         vec1 = self.embed(text1)
         vec2 = self.embed(text2)
         return self.cosine(vec1,vec2)
+
